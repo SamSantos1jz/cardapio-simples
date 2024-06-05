ce6c8456c5d55e12a63322a8aa38ca24ba616258
SamSantos1jz <samsantos1jz@gmail.com>
2024-06-05 01:25:08 -0300

    feat: api-whatsapp

--- script.js
+++ script.js
@@ -26,24 +26,21 @@ closeModalBtn.addEventListener("click", function(){
 })
 
 menu.addEventListener("click", function(event){
-    let parentButton = event.target.closest(".add-cart-btn")
+    let buttonCar = event.target.closest(".add-cart-btn")
 
-    if(parentButton){
-        const name = parentButton.getAttribute("data-name")
-        const price = parseFloat(parentButton.getAttribute("data-price"))
+    if(buttonCar){
+        const name = buttonCar.getAttribute("data-name")
+        const price = parseFloat(buttonCar.getAttribute("data-price"))
         addToCart(name, price)
     }
     
 })
 
-
 function addToCart(name, price){
     const existingItems = cart.find(item => item.name === name)
-   
+    
     if (existingItems){
-        existingItems.quantity +=1;
-        existingItems.price+=price
-        return 
+        existingItems.quantity += 1;
     } else {
         cart.push({
             name,
@@ -52,10 +49,137 @@ function addToCart(name, price){
         })
     }
     updateCartModal()
-  
+   
 }
-
 
 function updateCartModal(){
-    
+    cartItemsContainer.innerHTML= "";
+    let total = 0;
+
+    cart.forEach(item =>{
+        const cartItemsElement = document.createElement("div");
+        cartItemsElement.classList.add("flex","justify-between","mb-4","flex-col")
+        
+
+        cartItemsElement.innerHTML = `
+            <div class="flex items-center justify-between">
+                <div>
+                    <p class="font-bold">${item.name}</p>
+                    <p>Qtd: ${item.quantity}</p>
+                    <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+                </div>
+                   <button class="text-red-500" >
+                        <i class="remover fa-solid fa-trash-can" data-name='${item.name}'></i>
+                   </button>
+
+            </div>
+        `
+
+        total += item.price * item.quantity;
+
+        cartItemsContainer.appendChild(cartItemsElement);
+    })
+    //FORMATAÇÃO DO TOTAL EM BR
+    cartTotal.textContent = total.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
+
+    cartCounter.innerText = cart.length;
+
 }
+
+cartItemsContainer.addEventListener("click", function(event){
+    if(event.target.classList.contains("remover")){
+        const name = event.target.getAttribute("data-name")
+         removeItemsCart(name);
+    }
+})
+
+function removeItemsCart(name){
+    const index = cart.findIndex(item => item.name === name);
+
+    if( index !== -1){
+        const item = cart[index];
+        if(item.quantity > 1){
+            item.quantity-=1;
+            updateCartModal()
+            return
+        }
+        cart.splice(index, 1);
+        updateCartModal()
+    }
+}
+
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+
+})
+
+checkout.addEventListener("click", function(){
+    const isOpen = checkRestaurantOpen();
+    if(!isOpen){
+        Toastify({
+                text: "Ops, restaurante Fechado!!",
+                duration: 3000,
+                close: true,
+                gravity: "top", // `top` or `bottom`
+                position: "right", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                  background: "#ef4444",
+                },
+            }).showToast();
+        return;
+    }
+
+    if (cart.length === 0 ) return;
+
+    if(addressInput.value === ''){
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        return;
+    }
+
+    //API WHATS
+    const cartItems = cart.map((item) =>{
+        return (
+            `
+                ${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price} |
+            `
+    )
+    }).join("")
+
+    const message =  encodeURIComponent(cartItems)
+    const phone = "13982314149"
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço:${addressInput.value}`, "_blank");
+
+    cart = [];  
+    updateCartModal();
+})
+
+
+function checkRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours(); 
+    return hora > 18 && hora < 22;
+}
+
+const spanItem = document.getElementById("date-span");
+const isOpen = checkRestaurantOpen();
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-500")
+    spanItem.classList.add("bg-green-600")
+} else {
+    spanItem.classList.remove("bg-green-600")
+    spanItem.classList.add("bg-red-500")
+}
+
+
